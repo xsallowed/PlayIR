e10900118c4f8ac9d564a34d7d7b978417f128c9
xsallowed <soomrotahir@yahoo.com>
2021-02-05 23:51:59 +0000

    Update evidenceextractor.sh

--- evidenceextractor.sh
+++ evidenceextractor.sh
@@ -1,35 +1,6 @@
 #!/bin/bash
-#sudo apt-get install s3fs
-#mkdir ~/s3-drive
 read -p "Access key ID:" accesskey
-
 read -p  "Secret access key: " secretkey
-
 read -p "Enter S3 bucket name: " bucketname
-
 read -p "Evidence Folder: " evidencefolder
 
-echo $accesskey +':'+ $secretkey > ~/.passwd-s3fs
-
-chmod 600 .passwd-s3fs
-
-s3fs $bucketname ~/s3-drive
-
-sudo apt install p7zip-full p7zip-rar
-cd ~/s3-drive
-if $evidencefolder !="" 
-    then cd $evidencefolder
-fi
-
-for f in *.vmdk
-do
-	7z x $f -o/output/ '[SYSTEM]/*'
-	7z x $f -o/output/ 'Windows/System32/winevt/*'
-	7z x $f -o/output/ 'Users/*'
-	7z x $f -o/output/ 'Windows/System32/config'
-    7z x $f -o/output/ '*/History/*'
-    7z x $f -o/output/ '*/prefetch/*'
-    7z x f$ -o/output/ '*/appcompat/program/*'
-    7z x $f -o/output/ '*etl'
-done
-echo "DONE!!!"

f25deb55bfe8a0771fb6dcf3261c2d811b57b7bf
xsallowed <soomrotahir@yahoo.com>
2021-02-05 23:56:30 +0000

    Update evidenceextractor.sh

--- evidenceextractor.sh
+++ evidenceextractor.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-read -p "Access key ID:" accesskey
-read -p  "Secret access key: " secretkey
-read -p "Enter S3 bucket name: " bucketname
-read -p "Evidence Folder: " evidencefolder
-
+read -p "Enter access  key : "accesskey

173f190f1f4eec253a0a293de65892f4da95549e
xsallowed <soomrotahir@yahoo.com>
2021-02-06 23:51:17 +0000

    Update evidencecollector.sh

--- evidencecollector.sh
+++ evidencecollector.sh
@@ -23,12 +23,12 @@ for evidencefile in "$evidencepath"/*.vmdk
 do
 	hostname=$(basename "$evidencefile" .vmdk)
 	cd localoutput
-	mkdir "$hostname"
+	mkdir $hostname
 	cd ..
 	7z x "$evidencefile" -olocaloutput/"$hostname" '[SYSTEM]/*' 'Windows/System32/winevt/*' 'Users/*' 'Windows/System32/config' '*/History/*' '*/prefetch/*' '*/appcompat/program/*' '*etl'
-	7z a localoutput/"$hostname.zip" localoutput/"$hostname"/*
+	7z a localoutput/$hostname.zip localoutput/$hostname/*
 	mkdir s3drive/output
-	cp localoutput/"$hostname.7z" s3drive/output/
+	cp localoutput/$hostname.7z s3drive/output/
 done
 
 

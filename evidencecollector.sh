2d05e238dde2f9a2254d55e773a18e0dc89430d9
xsallowed <soomrotahir@yahoo.com>
2021-02-06 20:22:01 +0000

    Update evidencecollector.sh

--- evidencecollector.sh
+++ evidencecollector.sh
@@ -2,25 +2,29 @@
 read -p "Access key ID:" accesskey
 read -p  "Secret access key: " secretkey
 read -p "Enter S3 bucket name: " bucketname
-#read -p "Evidence Folder: " evidencefolder
-echo "$accesskey:$secretkey" > ~/.passwd-s3fs
+read -p "Evidence Folder: " evidencefolder
 
+echo "$accesskey:$secretkey" > .passwd-s3fs
 chmod 600 .passwd-s3fs
 mkdir s3-drive
-s3fs $bucketname ~/s3-drive
+s3fs $bucketname s3-drive
+
+evidencepath = "s3-drive"
+if [[-z"$evidencefolder"]] then 
+	 evidencepath = ${evidencepath}/${evidencefolder}
+fi
 
 sudo apt install p7zip-full p7zip-rar
 
 mkdir output
-#if [[-z"$evidencefolder"]] then 
-#	cd "$evidencefolder"
-#fi
-for evidencefile in *.vmdk
+
+for evidencefile in ${evidencepath}/*.vmdk
 do
-	mkdir "${evidencefile%.*}"
-	7z x s3-drive/$evidencefile -ooutput/"${evidencefile%.*}" '[SYSTEM]/*' 'Windows/System32/winevt/*' 'Users/*' 'Windows/System32/config' '*/History/*' '*/prefetch/*' '*/appcompat/program/*' '*etl'
-	7z a output/"${evidencefile%.*}" ./output/"${evidencefile%.*}"/*
-	cp "${evidencefile%.*}".7z s3-drive/
+	hostname = basename ${evidencefile} .vmdk
+	mkdir $hostname
+	7z x $evidencepath/${evidencefile} -ooutput/${hostname} '[SYSTEM]/*' 'Windows/System32/winevt/*' 'Users/*' 'Windows/System32/config' '*/History/*' '*/prefetch/*' '*/appcompat/program/*' '*etl'
+	7z a output/"${evidencefile%.*}".zip ./output/"${evidencefile%.*}"/*
+	cp "output/${evidencefile%.*}".7z s3-drive/
 done
 
 echo "DONE!!!"

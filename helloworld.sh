63175e4bcfc887335cfed1ae3c2c4abd19efa559
xsallowed <soomrotahir@yahoo.com>
2021-02-06 22:58:55 +0000

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-read -p "What is your name:" name
-echo "Hello Mr. $name"
+
+read -p "Evidence Folder: " evidencefolder
+
+evidencepath="s3drive"
+if [-z "$evidencefolder"] 
+then 
+	 evidencepath="$evidencepath"/"$evidencefolder"
+fi
+
+for evidencefile in "$evidencepath"/*.vmdk
+do
+  echo "evidence path : $evidencepath"
+  echo "host : $hostname"
+  echo "Evidence file : $evidencefile"	
+done
+

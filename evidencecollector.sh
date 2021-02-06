e04cb510f534f9a37bbf3e7bb85c92ccb40224b5
xsallowed <soomrotahir@yahoo.com>
2021-02-06 21:53:49 +0000

    Update evidencecollector.sh

--- evidencecollector.sh
+++ evidencecollector.sh
@@ -12,25 +12,25 @@ s3fs $bucketname s3drive
 evidencepath="s3drive"
 if [-z "$evidencefolder"] 
 then 
-	 evidencepath=${evidencepath}/${evidencefolder}
+	 evidencepath="$evidencepath"/"$evidencefolder"
 fi
 
 sudo apt install p7zip-full p7zip-rar
 
 sudo mkdir localoutput
 
-for evidencefile in ${evidencepath}/*.vmdk
+for evidencefile in "$evidencepath"/*.vmdk
 do
-	hostname=basename "${evidencefile}" .vmdk
-	mkdir "${hostname}"
-	7z x "$evidencefile" -o"localoutput/$hostname" '[SYSTEM]/*' 'Windows/System32/winevt/*' 'Users/*' 'Windows/System32/config' '*/History/*' '*/prefetch/*' '*/appcompat/program/*' '*etl'
-	7z a "localoutput/$hostname.zip" "localoutput/$hostname/*"
+	hostname=basename "$evidencefile" .vmdk
+	mkdir "$hostname"
+	7z x "$evidencefile" -olocaloutput/"$hostname" '[SYSTEM]/*' 'Windows/System32/winevt/*' 'Users/*' 'Windows/System32/config' '*/History/*' '*/prefetch/*' '*/appcompat/program/*' '*etl'
+	7z a localoutput/"$hostname.zip" localoutput/"$hostname"/*
 	mkdir s3drive/output
 	cp "localoutput/$hostname.7z" s3drive/output/
 done
 
 echo "evidence path : $evidencepath"
-echo "host : ${hostname}"
+echo "host : $hostname"
 echo "Evidence file : $evidencefile"
 
 echo "DONE!!!"

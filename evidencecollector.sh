19caed50908607c342faae38518f33a6cfc8b6bd
xsallowed <soomrotahir@yahoo.com>
2021-02-06 17:37:13 +0000

    Update evidencecollector.sh

--- evidencecollector.sh
+++ evidencecollector.sh
@@ -2,7 +2,7 @@
 read -p "Access key ID:" accesskey
 read -p  "Secret access key: " secretkey
 read -p "Enter S3 bucket name: " bucketname
-read -p "Evidence Folder: " evidencefolder
+#read -p "Evidence Folder: " evidencefolder
 echo "$accesskey:$secretkey" > ~/.passwd-s3fs
 
 chmod 600 .passwd-s3fs
@@ -10,13 +10,12 @@ chmod 600 .passwd-s3fs
 s3fs $bucketname ~/s3-drive
 
 sudo apt install p7zip-full p7zip-rar
-cd ~/s3-drive
-if [[-z"$evidencefolder"]] then 
-	cd $evidencefolder
-fi
-
+cd "~/s3-drive"
+#if [[-z"$evidencefolder"]] then 
+#	cd "$evidencefolder"
+#fi
 for f in *.vmdk
 do
-	7z x $bucketname/$f -o/output/ '[SYSTEM]/*'
+	7z x $f -o/output/ '[SYSTEM]/*'
 done
 echo "DONE!!!"

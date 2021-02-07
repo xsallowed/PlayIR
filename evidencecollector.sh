851153a192fb8c860602c3f9c0f80f328c5b8b80
xsallowed <soomrotahir@yahoo.com>
2021-02-07 17:24:44 +0000

    Update evidencecollector.sh

--- evidencecollector.sh
+++ evidencecollector.sh
@@ -4,7 +4,7 @@ read -p  "Secret access key: " secretkey
 read -p "Enter S3 bucket name: " bucketname
 read -p "Evidence Folder: " evidencefolder
 
-echo "$accesskey:$secretkey" > .passwd-s3fs
+echo "$accesskey:$secretkey" > ~/.passwd-s3fs
 chmod 600 .passwd-s3fs
 mkdir s3drive
 s3fs $bucketname s3drive
